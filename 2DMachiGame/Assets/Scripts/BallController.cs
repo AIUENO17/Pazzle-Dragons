f087968f15522333ca919c71142da8a0b4ba42df
AIUENO17 <lallapallooza_1120@au.com>
2021-02-14 16:31:25 +0900

    パズドラ

---
+++ 2DMachiGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BallController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+
+{
+
+    private bool IsTouch = false;
+
+    RectTransform RectTransform;
+
+    private void Awake()
+    {
+        RectTransform = this.GetComponent<RectTransform>();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (IsTouch)
+        {
+            RectTransform.position = Input.mousePosition;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        IsTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        IsTouch = false;
+    }
+
+
+    
+}
+   
+   
---
+++ 2DMachiGame/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject Ball;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(Ball,this.transform);
+        }
+    }
+
+
+}
